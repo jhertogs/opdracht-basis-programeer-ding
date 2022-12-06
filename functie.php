d27deeddc63d9d830415796f7cb1ffada1a333e0
jhertogs <97103567@st.deltion.nl>
2022-12-06 11:17:11 +0100

    functie

---
+++ functie.php
@@ -0,0 +1,16 @@
+<?php 
+
+function func() {
+    echo "+ deze regel is in func() uitgevoerd.\n";
+}
+
+echo "--Deze regel is voor func() uitgevoerd.\n";
+
+func();
+
+echo "== Deze regel is tussen 2 func() uitgevoerd.\n";
+
+func("");
+
+echo "-- Deze regel is na func() uitgevoerd.\n";
+?>
--- ifkeuze.php
+++ ifkeuze.php
@@ -1,9 +1,11 @@
 <?php 
-
+//variables
 $naam=readline("Geef jouw naam:");
 $leeftijd=readline("Geef houw leeftijd:");
 $getal=21;
 echo "Hallo $naam \n";
+
+//if's
 
 if ($leeftijd < $getal) {
     echo "Jij bent jonger dan $getal
