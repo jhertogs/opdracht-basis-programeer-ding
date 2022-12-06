8ef63b1062cf5bf3ab9449ee3d54829f4d1c3144
jhertogs <97103567@st.deltion.nl>
2022-12-06 21:40:24 +0100

    finished quiz

--- quiz.php
+++ quiz.php
@@ -1,39 +1,49 @@
 <?php 
+
+//variables & random
 $teller=0;
 $random=rand(0,100);
 $beurt=0;
 
+
+//de while loop (vraagt of je wilt starten)
 while ($teller <1) { 
 $input=readline("Wil je starten? druk op druk op een toets. En typ [stop] om te stoppen\n");
+
+//stopt de while loop als e stop wordt getypt
 if($input =="stop") {
         $teller++;
     }
 
+    //vaagt om getal te raden
 $input=readline("raad een nummer tussen 0 en 100\n");
   $beurt++;
+
+  //checked of het antwoord groter is dan random
 if($input > $random) {
     echo "Het antwoort is te groot\n";
-     
 }
+//checked of het antwoord kleiner is dan random
  if ($input < $random) {
     echo "Het antwoord is te klein\n";
 }
 
-
+//checked of het antwoord gelijk is aan random 
 if($input == $random) { 
     echo "Proficiat je hebt het goed!\n";
-    echo "je hebt er: $teller2 keer over gedaan\n";
+    echo "je hebt er: $teller keer over gedaan\n";
     $teller++;
 
   }  
-
+//checked of het antwoord kleiner is dan 1 en stopt programma als dat het geval is
   if($input < 1) {
   echo "Geen valide antwoord\n";
   $teller++;
   }
-
+  
+//checked of het antwoord groter is dan 100 en stopt programma als dat het geval is
   if($input > 100) {
-    echo "Geen valide antwoord\n";
+    echo "Geen valide antwoord";
     $teller++;
     }
     
