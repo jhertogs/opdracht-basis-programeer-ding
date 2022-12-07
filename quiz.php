afc036422acbe5ae7415f399ffe9dfae0db8047e
jhertogs <97103567@st.deltion.nl>
2022-12-07 12:17:10 +0100

    really finished quiz

--- quiz.php
+++ quiz.php
@@ -9,6 +9,7 @@ $beurt=0;
 //de while loop (vraagt of je wilt starten)
 while ($teller <1) { 
 $input=readline("Wil je starten? druk op druk op een toets. En typ [stop] om te stoppen\n");
+$beurt++;
 
 //stopt de while loop als e stop wordt getypt
 if($input =="stop") {
@@ -17,21 +18,23 @@ if($input =="stop") {
 
     //vaagt om getal te raden
 $input=readline("raad een nummer tussen 0 en 100\n");
-  $beurt++;
+  
 
   //checked of het antwoord groter is dan random
 if($input > $random) {
     echo "Het antwoort is te groot\n";
+    
 }
 //checked of het antwoord kleiner is dan random
  if ($input < $random) {
     echo "Het antwoord is te klein\n";
+    
 }
 
 //checked of het antwoord gelijk is aan random 
 if($input == $random) { 
     echo "Proficiat je hebt het goed!\n";
-    echo "je hebt er: $teller keer over gedaan\n";
+    echo "je hebt er: $beurt keer over gedaan\n";
     $teller++;
 
   }  
